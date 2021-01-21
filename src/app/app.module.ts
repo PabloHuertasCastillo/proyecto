49647cbc08db4600da7d1d6090ae700fcd2d2761
Pablo Huertas Castillo <pahucastle@gmail.com>
2021-01-21 16:14:55 +0100

    Version con loteria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './componentes/home/home.component';
 import { AboutComponent } from './componentes/about/about.component';
 import { FormsModule } from '@angular/forms';
+import { NavComponent } from './componentes/nav/nav.component';
+import { LoteriaComponent } from './componentes/loteria/loteria.component';
+import { GenerarLoteriaComponent } from './componentes/generar-loteria/generar-loteria.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    NavComponent,
+    LoteriaComponent,
+    GenerarLoteriaComponent
   ],
   imports: [
     BrowserModule,
